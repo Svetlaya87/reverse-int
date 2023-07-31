9262a5cd549c127dcfefaa9d165ed06761cffd2c
Svetlaya87 <svetlaya87@gmail.com>
2023-07-31 13:50:02 +0300

    feat: complit task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
-  
+    console.log(typeof n+" n");
+    if(n<0){
+        n= -n;
+    }
+
+    let number = (n+"").split("").map(x => x*1);
+    
+    
+    if(number[number.length-1] === 0){
+        number.splice(number.length-1, 1);
+    }
+    
+
+    number = +number.reverse().join("");
+    console.log(number+"+"+ typeof number+" number");
+    return number;
+    
 }
